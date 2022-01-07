810c21092233cd54185a0bf7f5ddd4a897c28c1e
Shin <heeyeon0705@gmail.com>
2022-01-07 09:08:06 +0900

    hidden, .preventDefault()

--- nomad/websiteClone.js
+++ nomad/websiteClone.js
@@ -1,16 +1,30 @@
-const h1 = document.querySelector(".hello:first-child h1")
+// const input = loginForm.querySelector("input")
+//               //login-from(id) 안에 존재하는 요소는 login-form에서 선택가능
+// const btn = loginForm.querySelector("button")
+const loginForm = document.querySelector('#login-form')
+const loginInput = document.querySelector("#login-form input")
+const greeting =  document.querySelector("#greeting")
 
-function handleh1Click(){
-  
-  const clickedClass = "active"
-  
-  if(h1.className === clickedClass){
-    h1.className = ""
-  }else{
-    h1.className = clickedClass
-  }
+
+const hidden_className = "hidden" 
+function onLoginSubmit(event){
+  event.preventDefault()
+  //.preventDefault() : 기본동작이 실행 되는 것을 막음 
+  loginForm.classList.add(hidden_className)
+  const username = loginInput.value
+
+  //h1 태그에 환영인사 입력
+  greeting.innerText = `Hello! ${username}`
+  // ``(백틱 사용으로 text와 변수명 한번에 사용)
+
+  //class 지우기
+  greeting.classList.remove(hidden_className)
 
 }
 
-h1.addEventListener("click",  handleh1Click)
-//addEventListener 선호 -> removeEvent 사용 가능 
+loginForm.addEventListener("submit", onLoginSubmit)
+
+
+
+
+
