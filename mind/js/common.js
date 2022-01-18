060a6bc1493dbba3901a941e32a5a4e420e99f89
Shin <heeyeon0705@gmail.com>
2022-01-18 17:11:20 +0900

    default

--- mind/js/common.js
+++ mind/js/common.js
@@ -2,12 +2,12 @@
 // 모바일 헤더 메뉴 
 const togle = document.querySelector('.headermenu_togle')
 const menu = document.querySelector('.dep1')
-const sub_menu = document.querySelector('.dep2')
+const sub_menu = document.querySelectorAll('.dep2')
 
 togle.addEventListener('click', ()=>{
     menu.classList.toggle('active')
 })
 
-menu.addEventListener('click', ()=>{
-    sub_menu.classList.toggle('active')
+menu.addEventListener('click',()=>{
+    
 })
