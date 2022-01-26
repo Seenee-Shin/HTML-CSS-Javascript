5c01ccb43d166118ee006b7f368025901e006e47
Shin <heeyeon0705@gmail.com>
2022-01-26 17:24:05 +0900

    220126 html

--- mind/comunity/js/comunity_freeboard.js
+++ mind/comunity/js/comunity_freeboard.js
@@ -1,6 +1,15 @@
 const write = document.querySelector("#free_board_write")
 const writing = document.querySelector("#post_textarea")
 
-if (window.matchMedia('(max-width: 768px)').matches){
-    writing.readOnly=true
+window.onresize = function(){
+    var innerWidth = window.innerWidth;
+    var innerHeight = window.innerHeight;
+    
+    if(innerWidth <= 768){
+        writing.readOnly=true
+        
+    }else{
+        writing.readOnly=false
+
+    }
 }
--- mind/comunity/js/comunity_worry_board.js
+++ mind/comunity/js/comunity_worry_board.js
@@ -5,10 +5,21 @@ const modal =  document.querySelector(".modal")
 const openModal = ()=>{
     modal.classList.remove("hidden")
 }
-
-const closeBtn = document.querySelector("#closeModal")
 const closeModal = ()=>{
     modal.classList.add("hidden")
 }
+const togleModal = ()=>{
+    category.classList.toggle("hidden")
+}
+
+const closeBtn = document.querySelector("#closeModal")
 openBtn.addEventListener("click", openModal)
 closeBtn.addEventListener("click", closeModal)
+
+/* 카테고리 여닫기 */
+const categoryBtn = document.querySelector(".worry_category")
+const category = document.querySelector(".worry_top_wrap + .worry_category_wrap")
+categoryBtn.addEventListener("click",togleModal)
+
+
+
